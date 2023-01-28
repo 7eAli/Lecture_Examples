05f0f5dacdf54085b2eee84911f5dc54af2437c7
GSY <gle722@yandex.ru>
2023-01-28 21:39:18 +0300

    Initial commit

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿int numA = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numB);
+Console.WriteLine(numA + numB);
