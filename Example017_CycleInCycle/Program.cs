296e8c1d2432398c594a6325503f2d03c59f9f17
GSY <gle722@yandex.ru>
2023-02-04 19:54:24 +0300

    Пример использования цикла в цикле

---
+++ Example017_CycleInCycle/Program.cs
@@ -0,0 +1,11 @@
+﻿// Пример работы цикла в цикле
+Console.Clear();
+Console.WriteLine("_____________");
+for (int i = 2; i <= 10; i++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i*j}");
+    }
+    Console.WriteLine("_____________");
+}
