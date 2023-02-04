c4dcffae3db522fd7e183ff29d24fa85d07c2088
GSY <gle722@yandex.ru>
2023-02-04 19:47:32 +0300

    Пример цикла for

---
+++ Example016_CycleFor/Program.cs
@@ -0,0 +1,14 @@
+﻿// Работа с циклом for
+
+string MethodTypeFour(int count, string text)
+{    
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string laugh = MethodTypeFour(5, "хахаха");
+Console.WriteLine(laugh);
