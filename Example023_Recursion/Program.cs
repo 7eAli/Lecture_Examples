ac496a07a9f563b518705a15961feadf68f7c58e
GSY <gle722@yandex.ru>
2023-02-11 19:47:57 +0300

    Пример нахождения факториала рекурсией

---
+++ Example023_Recursion/Program.cs
@@ -0,0 +1,9 @@
+﻿double Factorial(double number)
+{
+    if(number == 1 || number == 0) return 1;
+    else return number * Factorial(number - 1);
+}
+for (int i = 0; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
