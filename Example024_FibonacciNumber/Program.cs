8e4e997f815cf73ab2014eb85b917c5c0805f6a2
GSY <gle722@yandex.ru>
2023-02-11 19:58:33 +0300

    Пример с числами Фибоначи

---
+++ Example024_FibonacciNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int num)
+{
+    if(num == 1 || num == 2) return 1;
+    else return Fibonacci(num - 1) + Fibonacci(num - 2);
+}
+
+for (int i = 1; i < 100; i++)
+{
+    Console.WriteLine($"{i}:   {Fibonacci(i)}");
+}
